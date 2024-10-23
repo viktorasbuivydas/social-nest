f1ce21110844783a3774dbd9cacc30fab4f6a032
viktorasbuivydas <viktorasss00@gmail.com>
2024-10-24 00:14:58 +0300

    fixed vite error on tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,7 +4,13 @@ namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
+
 abstract class TestCase extends BaseTestCase
 {
-    //
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->withoutVite();
+    }
 }
